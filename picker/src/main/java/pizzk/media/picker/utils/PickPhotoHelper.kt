7facde2e62a0b8897039f885b0c0a7e48db4b141
mobile <976382156@qq.com>
2018-07-25 16:59:40 +0800

    提取PickPhotoHelper

---
+++ picker/src/main/java/pizzk/media/picker/utils/PickPhotoHelper.kt
@@ -0,0 +1,40 @@
+package pizzk.media.picker.utils
+
+import android.app.Activity
+import android.net.Uri
+import pizzk.media.picker.R
+import pizzk.media.picker.arch.CropParams
+import pizzk.media.picker.arch.PickControl
+import pizzk.media.picker.view.PickChoseActivity
+
+/**
+ * 默认图片拾取类
+ */
+object PickPhotoHelper {
+
+    fun show(activity: Activity, limit: Int = 1, cropParams: CropParams? = null,
+             callback: (action: Int, uris: List<Uri>) -> Unit) {
+        val choiceList: List<String> = listOf(
+                activity.getString(R.string.pick_chose_camera),
+                activity.getString(R.string.pick_chose_album)
+        )
+        PickChoseActivity.show(activity, choiceList) { key ->
+            val action: Int = when (key) {
+                choiceList[0] -> {
+                    PickControl.ACTION_CAMERA
+                }
+                choiceList[1] -> {
+                    PickControl.ACTION_ALBUM
+                }
+                else -> -1
+            }
+            if (action < 0) return@show
+            PickControl.obtain(true)
+                    .action(action)
+                    .limit(limit)
+                    .callback(callback)
+                    .crop(cropParams)
+                    .done(activity)
+        }
+    }
+}
